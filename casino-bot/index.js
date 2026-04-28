660061951b3e52baed19f92a6534a7abce27a5b8
YUDHA PERDANA <90470643+DhaBotz@users.noreply.github.com>
2026-04-28 09:19:31 +0700

    Update index.js

--- casino-bot/index.js
+++ casino-bot/index.js
@@ -1,1 +1,26 @@
 
+const makeWASocket = require("@whiskeysockets/baileys").default;
+const { useMultiFileAuthState } = require("@whiskeysockets/baileys");
+const pino = require("pino");
+
+const handler = require("./handler");
+
+async function start() {
+    const { state, saveCreds } = await useMultiFileAuthState("session");
+
+    const sock = makeWASocket({
+        logger: pino({ level: "silent" }),
+        auth: state,
+    });
+
+    sock.ev.on("creds.update", saveCreds);
+
+    sock.ev.on("messages.upsert", async (m) => {
+        const msg = m.messages[0];
+        if (!msg.message) return;
+
+        await handler(sock, msg);
+    });
+}
+
+start();
